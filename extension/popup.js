25822ca443ab2bd9e5502c3d8fc5311846e3576c
Kiran Kurian <kiran.kurian@rutgers.edu>
2015-02-08 00:32:30 -0500

    FUCK

--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,38 @@
+Parse.initialize("tlF96VIjmcQ7noWTyCcY7q2HQsIYuYKyNW3xsa5Y", "tNoWqeQHONKINhL8ot4GJNNDNNAZqpyRC7wWPqC1");
+
+$('#SignUp').on('submit', function(e){
+e.preventDefault();
+
+var User = Parse.Object.extend("User");
+var newUser = new User();
+
+newUser.save({
+  username: $("#username").val(),
+  password: $("#password").val(),
+  email: $("#email").val()
+},{
+  success: function(newUser){
+    console.log("Yo it saved");
+  }
+});
+});
+
+
+var checkUser = new Parse.Query("User");
+
+$('#Login').on('submit', function(e){
+e.preventDefault();
+
+Parse.User.logIn($("#login-username").val(), $('#login-password').val(), {
+  success: function(user) {
+    $('#error').hide();
+    $('#showMe').show();
+
+  },
+  error: function(user, error) {
+    // The login failed. Check error to see why.
+    $('#error').show();
+  }
+});
+});
 
